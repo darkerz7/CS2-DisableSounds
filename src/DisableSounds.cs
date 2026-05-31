5171758d93de4008a11ca38e5aff0de3ea3c56d1
darkerz7 <25752428+darkerz7@users.noreply.github.com>
2026-05-31 20:32:18 +0500

    Update 1.DZ.1
    Migration to .NET10

--- src/DisableSounds.cs
+++ src/DisableSounds.cs
@@ -1,13 +1,15 @@
 ﻿using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes;
 
 namespace CS2_DisableSounds
 {
+    [MinimumApiVersion(369)]
     public class DisableSounds : BasePlugin
 	{
 		public override string ModuleName => "Disable Sounds";
 		public override string ModuleDescription => "Allows you to turn off loud sounds";
 		public override string ModuleAuthor => "DarkerZ [RUS]";
-		public override string ModuleVersion => "1.0";
+		public override string ModuleVersion => "1.1";
 
 		public override void Load(bool hotReload)
 		{
@@ -17,7 +19,7 @@ namespace CS2_DisableSounds
 				
 				if (soundevent == 2149843356 ||	//R8
 					soundevent == 576815311 || //Glock and Famas
-					soundevent == 62938228 || soundevent == 2831007164 || soundevent == 3573863551 || soundevent == 3535174312 || soundevent == 1193078452 ||//HeadShot
+					soundevent == 62938228 || soundevent == 2831007164 || soundevent == 3573863551 || soundevent == 3535174312 || soundevent == 1193078452 || //HeadShot
 					soundevent == 3767841471 || soundevent == 524041390 || soundevent == 708038349) //Body
 				{
 					um.Recipients.Clear();
